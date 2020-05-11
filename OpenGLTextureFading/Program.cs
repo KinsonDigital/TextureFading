0f3437979dfa3b091a8030c7b104225814b52652
Calvin Wilkinson <calvin.wilkinson@meijer.com>
2020-05-11 19:56:13 -0400

    Add project files.

---
+++ OpenGLTextureFading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenGLTextureFading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SDLTextureFading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDLTextureFading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
